a818af18129283285553dbe791dd39559a592344
moguangping <moguangping@basecity.com>
2018-10-14 01:34:45 +0800

    修改示例和添加拓展方法。

---
+++ Assets/Scripts/LuaLibrary/TransformExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using XLua;
+
+namespace LuaLibrary
+{
+    [LuaCallCSharp]
+    public static class TransformExtension
+    {
+        public static Component GetChildComponent(this Transform rootTrans, string childName, Type compType)
+        {
+            Transform child = rootTrans.Find(childName);
+            return child.GetComponent(compType);
+        }
+    }
+}
