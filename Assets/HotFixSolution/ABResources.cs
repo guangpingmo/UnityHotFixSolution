cafc411287ab5bc80e46ae3ff1f0422f2e90ac75
moguangping <moguangping@basecity.com>
2018-10-14 09:16:32 +0800

    增加Lua调用加载资源的API。

--- Assets/HotFixSolution/ABResources.cs
+++ Assets/HotFixSolution/ABResources.cs
@@ -25,31 +25,47 @@ namespace HotFixSolution
 
         public static T Load<T>(string assetPath) where T : UnityEngine.Object
         {
-            Debug.LogFormat("ABResources.Load assetPath:{0} type:{1}", assetPath, typeof(T).FullName);
+            UnityEngine.Object obj = Load(assetPath, typeof(T));
+            return obj as T;
+        }
+
+        public static UnityEngine.Object Load(string assetPath, Type type)
+        {
+            Debug.LogFormat("ABResources.Load assetPath:{0} type:{1}", assetPath, type.FullName);
             if (Application.isEditor)
             {
 #if UNITY_EDITOR
-                if(EmulateLoadAssetBundleInEditor) {
-                    return LoadAssetFromAssetBundle<T>(assetPath);
-                } else {
-                    return AssetDatabase.LoadAssetAtPath<T>(assetPath);
+                if (EmulateLoadAssetBundleInEditor)
+                {
+                    return LoadAssetFromAssetBundle(assetPath, type);
+                }
+                else
+                {
+                    return AssetDatabase.LoadAssetAtPath(assetPath, type);
                 }
 #else
                 return null;
 #endif
-            } else {
-                return LoadAssetFromAssetBundle<T>(assetPath);
+            }
+            else
+            {
+                return LoadAssetFromAssetBundle(assetPath, type);
             }
         }
 
-        private static T LoadAssetFromAssetBundle<T>(string assetPath) where T : UnityEngine.Object
+        private static UnityEngine.Object LoadAssetFromAssetBundle(string assetPath, Type type)
         {
             string abName = AssetPathToAssetBundleName(assetPath);
             AssetBundle assetBundle = ABManager.LoadAssetBundle(abName);
             if (assetBundle == null)
                 throw new Exception(string.Format("fail to Load AssetBundle For {0}!", assetPath));
-            T asset = assetBundle.LoadAsset<T>(assetPath);
-            return asset;
+            return assetBundle.LoadAsset(assetPath, type);
+        }
+
+        private static T LoadAssetFromAssetBundle<T>(string assetPath) where T : UnityEngine.Object
+        {
+            UnityEngine.Object obj = LoadAssetFromAssetBundle(assetPath, typeof(T));
+            return obj as T;
         }
 
         public static string AssetPathToAssetBundleName(string assetPath)
